59513c4338a122a62a1c91e96f4e1752d77a5d73
FabianUM <fabian.israel.07@gmail.com>
2024-09-06 12:09:41 -0500

    saber quienes son mis amigos

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,7 +9,10 @@ class PageController extends Controller
 {
     public function dashboard(Request $request)
     {
-
+        dd(
+            $request->user()->friendsFrom()->get(),
+            $request->user()->friendsTo()->get()
+        );
         if ($request->get('for-my')) {
             $posts = $request->user()->posts()->latest()->get();
         } else {
