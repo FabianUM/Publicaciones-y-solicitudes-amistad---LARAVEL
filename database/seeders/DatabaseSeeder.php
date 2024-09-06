a7e6684ee351bf478cafba749369277ab0aaf5d1
FabianUM <fabian.israel.07@gmail.com>
2024-09-06 09:54:03 -0500

    configuracion de datos semilla

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,9 +15,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        /*\App\Models\User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+         ]);*/
+
+        User::factory()->create(['email' => 'f@test.com']);
+
+        User::factory(19)->hasPosts(4)->create();
     }
 }
