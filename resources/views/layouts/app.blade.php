74baa795aa1d8e3cd4bf9cb93f302a60f5723136
FabianUM <fabian.israel.07@gmail.com>
2024-09-06 14:55:35 -0500

    ajustes finales

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Sistema de Publicaciones</title>
+        <title>Sistema de Publicaciones - Fabian</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
